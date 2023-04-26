abb9cfa66d8791f2161816a26b3892bb339cb98d
ayush talpelwar <ayushtalpelwar@gmail.com>
2023-04-26 18:16:09 +0530

    day 13

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,32 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:thirty_days_of_flutter/models/catalog.dart';
 import 'package:thirty_days_of_flutter/widgets/drawer.dart';
 
 import '../widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async{
+    var catjson=await rootBundle.loadString("assets/files/catalog.json");
+    var decodeddata=jsonDecode(catjson);
+    var productsdata=decodeddata["products"];
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
