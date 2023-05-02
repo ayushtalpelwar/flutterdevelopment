f0f8cb0989742abe5be940612c80e45b5939e59e
ayush talpelwar <ayushtalpelwar@gmail.com>
2023-05-02 21:02:50 +0530

    day 16

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -15,24 +15,25 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   @override
   void initState() {
     super.initState();
     loadData();
   }
 
-  loadData() async{
+  loadData() async {
     await Future.delayed(Duration(seconds: 2));
-    var catjson=await rootBundle.loadString("assets/files/catalog.json");
-    var decodeddata=jsonDecode(catjson);
-    var productsdata=decodeddata["products"];
-    catalogModel.items=List.from(productsdata).map<Item>((item) => Item.fromMap(item)).toList();
+    var catjson = await rootBundle.loadString("assets/files/catalog.json");
+    var decodeddata = jsonDecode(catjson);
+    var productsdata = decodeddata["products"];
+    catalogModel.items = List.from(productsdata)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
     setState(() {});
   }
+
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: Text("30 Days Of Flutter"),
@@ -40,14 +41,24 @@ class _HomePageState extends State<HomePage> {
       drawer: MyDrawer(),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-          itemCount: catalogModel.items.length,
-          itemBuilder: (context,index){
-            return itemWidget(
-              item:catalogModel.items[index],
-            );
-          },
-        ),
+        child: GridView.builder(
+            gridDelegate:
+                SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2,mainAxisSpacing: 15,crossAxisSpacing: 10),
+
+            itemBuilder: (context, index) {
+              final item = catalogModel.items[index];
+              return Card(
+                clipBehavior: Clip.antiAlias,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15)),
+                child: GridTile(
+                  header: Text(item.name),
+                  child: Image.network(item.image),
+                  footer: Text(item.price.toString()),
+                ),
+              );
+            },
+            itemCount: catalogModel.items.length,),
       ),
     );
   }
